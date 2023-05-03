87c838e761212d3460edc7b2edce215fca8c12b0
Kushagra Raina <rainak@cardiff.ac.uk>
2023-05-03 15:32:51 +0100

    adding eda scripts for sample images and shapes

--- Preprocessing/eda.py
+++ Preprocessing/eda.py
@@ -28,21 +28,22 @@ def eda(folder_name):
 
 ## Image augmentation example
 def sample_images(file_name):
-    ## sample augmented images
+
     deskpath = r'C:\Users\c22056054\OneDrive - Cardiff University\Desktop'
     img = Image.open(file_name)
     img.save(os.path.join(deskpath, 'orig_img.jpg'))
 
+    # Augmentation type I
     augmented_img = T.GaussianBlur(kernel_size=(51, 91), sigma=randrange(1, 3))(img)
     augmented_img = T.RandomRotation(degrees=randrange(15, 90))(augmented_img)
-
     augmented_img.save(os.path.join(deskpath, 'aug1.jpg'))
-    idx += 1
+
+    # Augmentation type II
     augmented_img2 = T.ColorJitter()(img)
     augmented_img2 = T.RandomErasing(p=0.7, scale=(0.025, 0.5), ratio=(0.3, 2))(augmented_img2)
     augmented_img2.save(os.path.join(deskpath, 'aug2.jpg'))
-
-    # if len(os.listdir(os.path.join(folder_name, clas)))<min_class_size//3:
+t
+    # Augmentation type III
     augmented_img3 = T.ElasticTransform(alpha=50.0, sigma=5.0)(img)
     augmented_img3 = T.RandomHorizontalFlip()(augmented_img3)
     augmented_img3 = T.RandomVerticalFlip()(augmented_img3)
@@ -50,8 +51,8 @@ def sample_images(file_name):
 
 
 if __name__ == '__main__':
-    shapes = eda(r'C:\Users\c22056054\OneDrive - Cardiff University\Desktop\SM\Semester-II\Applications of Machine ' \
-                 r'Learning\datasets_coursework2\Flickr\Flickr')
+    # shapes = eda(r'C:\Users\c22056054\OneDrive - Cardiff University\Desktop\SM\Semester-II\Applications of Machine ' \
+    #              r'Learning\datasets_coursework2\Flickr\Flickr')
 
     sample_images(r'C:\Users\c22056054\OneDrive - Cardiff University\Desktop\SM\Semester-II\Applications of Machine '
-                  r'Learning\datasets_coursework2\Flickr\Flickr\fear\fear_0000.jpg')
+                  r'Learning\datasets_coursework2\Final_FI\fear\fear_0000.jpg')
