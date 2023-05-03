4549f0571e3fb230c3da34d1fba4e2d4c719b061
Kushagra Raina <rainak@cardiff.ac.uk>
2023-05-03 16:35:14 +0100

    added argparser and comments to augmentation script

--- Preprocessing/augment.py
+++ Preprocessing/augment.py
@@ -1,54 +1,43 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 import os
-import sys
-import time
-import torch
 import random
 import shutil
-import matplotlib.pyplot as plt
+import argparse
 import torchvision.transforms as T
 
 from PIL import Image
+from tqdm import tqdm
 from random import randrange
 
-# from torchvision.datasets import ImageFolder
 
-
-if __name__ == '__main__':
-
-    random.seed(10)
-    min_class_size = 3000
-    img_shape = (224, 224)
-    folder_name = r'C:\Users\c22056054\OneDrive - Cardiff University\Desktop\SM\Semester-II\Applications of Machine Learning\datasets_coursework2\Normalized_FI'
-    save_folder_name = r'C:\Users\c22056054\OneDrive - Cardiff University\Desktop\SM\Semester-II\Applications of Machine Learning\datasets_coursework2\Final_FI'
-    if not os.path.isdir(save_folder_name):
+def augment(source_folder, destination_folder, min_class_size, img_shape):
+    # Check if destinationfolder exists
+    if not os.path.isdir(destination_folder):
         print('Creating Augmented images dataset...')
-        os.mkdir(save_folder_name)
+        os.mkdir(destination_folder)
     else:
         print('Found Existing directory!')
-    classes = os.listdir(folder_name)
+    classes = os.listdir(source_folder)
 
+    # list classes in dataset
     for clas in classes:
-
-        if not os.path.isdir(os.path.join(save_folder_name, clas)):
+        if clas != 'fear':
+            continue
+        if not os.path.isdir(os.path.join(destination_folder, clas)):
             print('Creating folder of class', clas)
-            os.mkdir(os.path.join(save_folder_name, clas))
+            os.mkdir(os.path.join(destination_folder, clas))
 
-
-        class_name = os.path.join(folder_name, clas)
+        # class path
+        class_name = os.path.join(source_folder, clas)
         files = os.listdir(class_name)
         idx = len(files)
         random.shuffle(files)
-
+        print('Augmenting for Class', clas)
         # Copy pasting images from the normalized dataset
-        for file in files[:min_class_size]:
-            shutil.copy(os.path.join(class_name, file), os.path.join(save_folder_name, clas, file))
+        for file in tqdm(files[:min_class_size]):
+            shutil.copy(os.path.join(class_name, file), os.path.join(destination_folder, clas, file))
 
-        for file in files:
+        # open images in class
+        for file in tqdm(files):
 
             if idx >= min_class_size:
                 print('3000 images extracted for class', clas)
@@ -57,30 +46,41 @@ if __name__ == '__main__':
             file_name = os.path.join(class_name, file)
 
             img = Image.open(file_name)
-            augmented_img = T.GaussianBlur(kernel_size=(51, 91), sigma=randrange(1, 3))(img)
-            augmented_img = T.RandomRotation(degrees=randrange(15, 90))(augmented_img)
-            # print('augmenting image:', file)
-            # the below line is prone to errors for idx less than 1000, but
-            # it will work for us since all folders already have more than 1000 files
-            save_aug_file = os.path.join(save_folder_name, clas, file_name.split('.')[0][:-4] + str(idx) + '.jpg')
-            idx+=1
-            if len(os.listdir(os.path.join(folder_name, clas)))<min_class_size//2:
-                augmented_img2 = T.ColorJitter()(img)
-                augmented_img2 = T.RandomErasing(p=0.7, scale=(0.025, 0.5), ratio=(0.3, 2))(augmented_img2)
-                save_aug_file2 = os.path.join(save_folder_name, clas, file_name.split('.')[0][:-4] + str(idx) + '.jpg')
-                idx+=1
-                augmented_img.save(save_aug_file2)
+            aug_img1 = T.GaussianBlur(kernel_size=(51, 91), sigma=randrange(1, 3))(img)
+            aug_img1 = T.RandomRotation(degrees=randrange(15, 90))(aug_img1)
+            aug1_file = os.path.join(destination_folder, clas, file_name.split('.')[0][:-4] + str(idx) + '.jpg')
+            aug_img1.save(aug1_file)
+            idx += 1
 
-            if len(os.listdir(os.path.join(folder_name, clas)))<min_class_size//3:
-                augmented_img3 = T.ElasticTransform(alpha=50, sigma=5)(img)
-                augmented_img3 = T.RandomHorizontalFlip()(augmented_img3)
-                augmented_img3 = T.RandomVerticalFlip()(augmented_img3)
-
-                save_aug_file3 = os.path.join(save_folder_name, clas, file_name.split('.')[0][:-4] + str(idx) + '.jpg')
+            if len(os.listdir(os.path.join(destination_folder, clas))) < min_class_size // 2:
+                aug_img2 = T.ColorJitter()(img)
+                aug_img2 = T.RandomErasing(p=0.7, scale=(0.025, 0.5), ratio=(0.3, 2))(aug_img2)
+                aug2_file = os.path.join(destination_folder, clas, file_name.split('.')[0][:-4] + str(idx) + '.jpg')
                 idx += 1
-                augmented_img.save(save_aug_file3)
-            # augmented_img.show()
+                aug_img2.save(aug2_file)
 
-            # if augmented_img.shape != (224, 224):
-            #     print('Image corrupted!')
-            #     sys.exit(0)
+            if len(os.listdir(os.path.join(destination_folder, clas))) < min_class_size // 3:
+                aug_img3 = T.ElasticTransform(alpha=50, sigma=5)(img)
+                aug_img3 = T.RandomHorizontalFlip()(aug_img3)
+                aug_img3 = T.RandomVerticalFlip()(aug_img3)
+
+                aug3_file = os.path.join(destination_folder, clas, file_name.split('.')[0][:-4] + str(idx) + '.jpg')
+                idx += 1
+                aug_img3.save(aug3_file)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+            prog='augment.py',
+            description='Augments dataset')
+
+    parser.add_argument('-i', '--input_folder', default=r'C:\Users\c22056054\OneDrive - Cardiff University\Desktop\SM\Semester-II\Applications of Machine Learning\datasets_coursework2\Normalized_FI')
+    parser.add_argument('-s', '--seed', default=10, type=int)
+    parser.add_argument('-o', '--output_folder', default=r'C:\Users\c22056054\OneDrive - Cardiff University\Desktop\SM\Semester-II\Applications of Machine Learning\datasets_coursework2\Final_FI')
+    parser.add_argument('-m', '--min_class_size', default=3000, type=int)
+    args = parser.parse_args()
+
+    random.seed(args.seed)
+    img_shape = (224, 224)
+
+    augment(args.input_folder, args.output_folder, args.min_class_size, img_shape)
